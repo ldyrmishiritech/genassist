559a05d18b04a0d12a73ffa99b8a07424ef78353
Eledi Dyrkaj <edyrkaj@ritech.co>
2026-01-19 17:26:19 +0100

    chore: update package-lock and yarn.lock for dependency management; refactor App component to utilize dynamic baseUrl and apiKey

--- plugins/react/example-app/src/App.tsx
+++ plugins/react/example-app/src/App.tsx
@@ -39,6 +39,11 @@ function App() {
     description: "Support",
     agentName: "Agent",
     logoUrl: "",
+    baseUrl: "http://localhost:8000/",
+    apiKey: "genagent123",
+    // baseUrl: "https://fibrously-lovelorn-porfirio.ngrok-free.dev/",
+    // apiKey: "FA40KmEYi17WgBnSH0mi4f25bSe4gZsWZ2JSjKW86eM6QOVg-WKWBQ",
+    // reCaptchaKey: "6LeLh08sAAAAAG_pJBb3KwgqRnqVmLRP6cTYjUIX",
   });
 
   const [customLogo, setCustomLogo] = useState<FileState>({
@@ -940,8 +945,8 @@ function App() {
       {/* Chat Widget Popup */}
       <div style={chatWidgetStyle}>
         <GenAgentChat
-          baseUrl="http://localhost:8000/"
-          apiKey="genagent123"
+          baseUrl={chatSettings.baseUrl}
+          apiKey={chatSettings.apiKey}
           tenant=""
           metadata={metadata}
           theme={theme}
@@ -949,6 +954,7 @@ function App() {
           agentName={chatSettings.agentName}
           logoUrl={chatSettings.logoUrl}
           onError={handleError}
+          reCaptchaKey={chatSettings.reCaptchaKey}
         />
       </div>
 
